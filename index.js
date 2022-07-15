e573e701805d8c5c020dcc510a5f807c4c61c328
rikifujihara <rikifujihara@gmail.com>
2022-07-15 12:41:48 +1000

    add React script tags to HTML head

--- index.js
+++
@@ -1,9 +0,0 @@
-const incrementBtn = document.getElementById('increment-btn')
-const countDisplay = document.getElementById('count-display')
-
-let currentCount = 0
-
-incrementBtn.addEventListener("click", () => {
-    currentCount++
-    countDisplay.innerText = currentCount
-})
