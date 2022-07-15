5df1d972db3efac780af0af31a6e22732575e794
rikifujihara <rikifujihara@gmail.com>
2022-07-15 19:05:08 +1000

    button count incremement with React

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CountButton from './CountButton'
+
+const App = () => {
+  
+    return (
+      <div>
+      
+        <CountButton />
+      </div>
+    )
+  }
+
+export default App
